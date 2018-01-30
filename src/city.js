1183ad141994165461ffcc178a78505da9850ce8
generateui <rtimon@gmail.com>
2018-01-30 21:19:28 +0100

    regen

---
+++ src/city.js
@@ -0,0 +1,6 @@
+class City {
+    constructor(player, node) {
+        this.player = player;
+        this.node = node;
+    }
+}
---
+++ src/town.js
@@ -0,0 +1,6 @@
+class Town {
+    constructor(player, node) {
+        this.node = node;
+        this.player = player;
+    }
+}
